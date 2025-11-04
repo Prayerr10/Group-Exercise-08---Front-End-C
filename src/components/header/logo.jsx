8e01c2f48d54d945bf91ff0686636afa75ff164f
jordyee <gerungan.dj@gmail.com>
2025-11-04 12:59:06 +0700

    making logo

---
+++ src/components/header/logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'; // Kita akan pakai file style dari induknya
+
+// 1. Impor gambar logo Anda
+import logoGambar from '../../assets/logo.png'; 
+// (Sesuaikan path '../../assets/logo.png' jika Anda menyimpannya di tempat lain)
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      {/* 2. Tampilkan gambar itu */}
+      <img src={logoGambar} alt="App Logo" className="header-logo" />
+    </div>
+  );
+}
+
+export default Logo;
